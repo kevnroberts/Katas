a25377379acbcacfcebef77d8a89a8269156fee3
Kevn Roberts <kev@thebrowntrout.net>
2012-07-06 22:49:43 -0600

    binary chop v. 2 using recursion

--- binary-chop/chop2.js
+++ binary-chop/chop2.js
@@ -3,5 +3,21 @@ window.chop = window.chop || [];
 
 window.chop[2] = function chop2(find, arr){
 	var indx = -1;
-	return indx;
+	var half, val;
+	if(arr.length <= 1){
+		if(arr[0] === find){
+			return 0;
+		} else {
+			return -1;
+		}
+	}
+
+	half = parseInt((arr.length) / 2, 10);
+	if(find < arr[half]){
+		return chop2(find, arr.slice(0, half));
+	} else {
+		val = chop2(find, arr.slice(half));
+		val = val >= 0 ? half + val : val;
+		return val;
+	}
 };
